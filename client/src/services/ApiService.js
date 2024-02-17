1dfa78857ea989affad5166be856fa90330114f5
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-17 17:45:17 -0500

    Update ApiService.js

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://grull-backend.onrender.com/';
 
 export const register = (username, password, specialization) => {
   return axios.post(`${API_URL}/api/register`, {
