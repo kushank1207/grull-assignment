4f074dc0ffa27051312dbafe708f2ff03151ae8a
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-17 16:19:40 -0500

    adjusting files for deployment

--- server/app/config.py
+++ server/app/config.py
@@ -2,5 +2,5 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///grull.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://peerjob_citizenhot:54aab961e028ec405636bfc4739050391c2604dc@v-s.h.filess.io:3306/peerjob_citizenhot'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
