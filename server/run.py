dba84a8dd3e8c0c1c8c99a3e7a464a2cc7a91c52
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-17 17:24:03 -0500

    Update run.py

--- server/run.py
+++ server/run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
+from dotenv import load_dotenv
+load_dotenv() 
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.getenv('FLASK_RUN_HOST'),port=int(os.getenv('FLASK_RUN_PORT')))
