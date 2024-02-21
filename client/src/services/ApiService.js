2c8c3c82b87a98e4995f49d4c2ffcf2de090ef96
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-20 23:33:08 -0500

    endpoint update

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = 'http://127.0.0.1:8000';
-const API_URL = 'https://grull-backend.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 
 export const register = (username, password, specialization) => {
