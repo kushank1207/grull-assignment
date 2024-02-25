769868f5fd9b4ee4b8981c3af004e6045147a440
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-24 19:46:39 -0500

    Swagger for docs

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,33 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flasgger import Swagger
 import os
 from dotenv import load_dotenv
-load_dotenv() 
+
+load_dotenv()
 
 db = SQLAlchemy()
-
 jwt = JWTManager()
 
 def create_app():
-    """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
     app.config.from_pyfile('config.py')
-    # print("Database URI1:", os.environ.get("DB_SECRET_KEY"))
-    # print("Database URI:", os.environ.get("SQLALCHEMY_DATABASE_URI"))
     migrate = Migrate(app, db)
 
     db.init_app(app)
-    
     jwt.init_app(app)
+
+    # Initialize Flasgger with your Flask app
+    Swagger(app, template_file='static/swagger.yaml')
 
     with app.app_context():
         from .models import User, Quest
         db.create_all()
 
-    from .api import api_bp  # Import Blueprint
+    from .api import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
